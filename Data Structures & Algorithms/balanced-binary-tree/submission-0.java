0e05329a5f54a10283c773479b497135ce9a873b
md hasan <144601779+mnah05@users.noreply.github.com>
2026-06-19 13:13:16 +0530

    Add: balanced-binary-tree - submission-0

---
+++ Data Structures & Algorithms/balanced-binary-tree/submission-0.java
@@ -0,0 +1,28 @@
+class Solution {
+
+    private int dfs(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+
+        int left = dfs(root.left);
+        if (left == -1) {
+            return -1;
+        }
+
+        int right = dfs(root.right);
+        if (right == -1) {
+            return -1;
+        }
+
+        if (Math.abs(left - right) > 1) {
+            return -1;
+        }
+
+        return 1 + Math.max(left, right);
+    }
+
+    public boolean isBalanced(TreeNode root) {
+        return dfs(root) != -1;
+    }
+}
