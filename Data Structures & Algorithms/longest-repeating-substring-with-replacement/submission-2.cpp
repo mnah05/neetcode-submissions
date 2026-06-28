bbd13d409681df860e41314d883742d5dc9a3b99
md hasan <144601779+mnah05@users.noreply.github.com>
2026-06-28 14:53:11 +0530

    Add: longest-repeating-substring-with-replacement - submission-2

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-2.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int characterReplacement(string s, int k) {
+        unordered_map<char, int> freq;
+        int l = 0;
+        int maxFreq = 0;
+        int ans = 0;
+
+        for (int r = 0; r < s.size(); r++) {
+            freq[s[r]]++;
+            maxFreq = max(maxFreq, freq[s[r]]);
+
+            while (((r - l + 1) - maxFreq) > k) {
+                freq[s[l]]--;
+                l++;
+            }
+
+            ans = max(ans, r - l + 1);
+        }
+
+        return ans;
+    }
+};
