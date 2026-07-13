15c5eb721769dd7f64374a83d1e633d53592055a
md hasan <144601779+mnah05@users.noreply.github.com>
2026-07-13 11:16:27 +0530

    Add: climbing-stairs - submission-0

---
+++ Data Structures & Algorithms/climbing-stairs/submission-0.cpp
@@ -0,0 +1,20 @@
+class Solution {
+   public:
+    int sol(int n, vector<int>& dp) {
+        if (n < 0) {
+            return 0;
+        }
+        if (n == 0) {
+            return 1;
+        }
+        if (dp[n] != -1) {
+            return dp[n];
+        }
+
+        return dp[n] = sol(n - 1,dp) + sol(n - 2,dp);
+    }
+    int climbStairs(int n) {
+        vector<int> dp(n + 1, -1);
+        return sol(n, dp);
+    }
+};
