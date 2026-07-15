bb36d39f8b269d9b8ff534b2b82c2638ed927773
md hasan <144601779+mnah05@users.noreply.github.com>
2026-07-15 11:24:21 +0530

    Add: word-break - submission-2

---
+++ Data Structures & Algorithms/word-break/submission-2.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    bool wordBreak(string s, vector<string>& wordDict) {
+        unordered_set<string> wordSet(wordDict.begin(), wordDict.end());
+
+        vector<int> memo(s.size(), -1);
+
+        return dfs(s, wordSet, 0, memo);
+    }
+
+    bool dfs(const string& s, const unordered_set<string>& st, int i, vector<int>& memo) {
+        if (i == s.size()) {
+            return true;
+        }
+
+        if (memo[i] != -1) {
+            return memo[i];
+        }
+
+        for (int j = i; j < s.size(); j++) {
+            string word = s.substr(i, j - i + 1);
+
+            if (st.find(word) != st.end()) {
+                if (dfs(s, st, j + 1, memo)) {
+                    return memo[i] = true;
+                }
+            }
+        }
+
+        return memo[i] = false;
+    }
+};
